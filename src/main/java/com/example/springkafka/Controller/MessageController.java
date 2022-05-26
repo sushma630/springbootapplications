e92dec3017ba637feb6c736bc48da31b5cd3388e
sushma <sushma630.t@gmail.com>
2022-05-26 10:58:27 +0530

    "kafka application"

---
+++ src/main/java/com/example/springkafka/Controller/MessageController.java
@@ -0,0 +1,27 @@
+package com.example.springkafka.Controller;
+
+import com.example.springkafka.producer.KafkaProducer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MessageController {
+
+    @Autowired
+    private KafkaProducer kafkaProducer;
+
+    public MessageController(KafkaProducer kafkaProducer) {
+        this.kafkaProducer = kafkaProducer;
+    }
+
+    @GetMapping("/publish")
+    public ResponseEntity<String> publish(@RequestParam("message") String message)
+    {
+        kafkaProducer.sendMessage(message);
+        return ResponseEntity.ok("message sent successfully");
+    }
+}
